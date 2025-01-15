6289b427aa35e1e6b280ffd7e2de3bfb48e36961
common1 <my.common.developer@gmail.com>
2025-01-15 11:21:01 +0100

    05 Write Your First Solidity Smart Contract | Complete Solidity Programming Language Course - Part 3

--- hello.sol
+++ hello.sol
@@ -4,4 +4,15 @@ pragma solidity 0.8.26;
 
 contract MyFirstContract {
     string public hey;
+    uint256 public no;
+
+    // constructor(string memory _hey, uint _no) {
+    //     hey = _hey;
+    //     no = _no;
+    // }
+
+    function addInfo(string memory _hey, uint _no) public {
+        hey = _hey;
+        no = _no;
+    }
 }

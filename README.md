fde7c192eaa441616335ae6380bbc3990b806252
common1 <my.common.developer@gmail.com>
2025-01-16 13:26:30 +0100

    07 Solidity Data Types | Data Types in Solidity Smart Contracts | Full Solidity Course - Part 2

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ https://www.youtube.com/watch?v=3Fr4UdCyQVE&list=PLWUCKsxdKl0oksYr6IG_wRsaSUySQC
 ```
 https://www.youtube.com/watch?v=YjmCioAa-wU&list=PLWUCKsxdKl0oksYr6IG_wRsaSUySQC0ck&index=6
 
-## 07 Solidity Data Types | Data Types in Solidity Smart Contracts | Full Solidity Corse
+## 07 Solidity Data Types | Data Types in Solidity Smart Contracts | Full Solidity Course
 ```
 https://www.youtube.com/watch?v=-YW2h8iYrYw&list=PLWUCKsxdKl0oksYr6IG_wRsaSUySQC0ck&index=7
 
--- dataTypes.sol
+++ dataTypes.sol
@@ -3,14 +3,31 @@
 pragma solidity 0.8.26;
 
 contract DataTypes {
-    bool public hey;
-    bool public no = true;
+    // bool public hey;
+    // bool public no = true;
 
     // uint8   // ranges 0 - 2 ** n8 - 1 // 255
     // uint16  // ranges 0 - 2 ** 16 - 1 // 65535
     // uint256 // ranges 0 - 2 ** 256 - 1 // 1.1579209+e77
 
-    uint8 public u8 = 1;
-    uint256 public u256 = 456;
-    uint public u = 123;
+    // int8 public i8 = -1;
+    // uint256 public u256 = 456;
+    // uint public u = 123;
+
+    // int public minInt = type(int).min;
+    // int public maxInt = type(int).max;
+
+    // bytes1 public a;
+    // bytes1 public b;
+
+    // bytes1 public a = 0xb5;
+    // bytes1 public b = 0x56;
+
+    // address public hey;
+    // address public addr = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+
+    bool public defaultBool;
+    uint public number;
+    int public defaultInt;
+    address public hey;
 }

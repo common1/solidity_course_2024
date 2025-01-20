3427787c47c8a4c2fdf8ce3ce321b744a36b71bd
common1 <my.common.developer@gmail.com>
2025-01-20 13:13:52 +0100

    36 Solidity Inheritance | How To Create Inheritance In Solidity From Parent Contract | Solidity Course - Part 2

--- inheritance.sol
+++ inheritance.sol
@@ -11,7 +11,7 @@ pragma solidity 0.8.26;
 
 */
 contract A {
-    function foo() public virtual returns (string memory) {
+    function foo() public pure virtual returns (string memory) {
         return "A";
     }
 }
@@ -31,3 +31,27 @@ contract C is A {
     }
 }
 
+contract D is B, C {
+    // D.foo() returns "C"
+    // since C is the right most parent contract with function foo()
+    function foo() public pure override(B, C) returns (string memory) {
+        return super.foo();
+    }
+}
+
+contract E is C, B {
+    // E.foo() returns "B"
+    // since B is the right most parent contract with function foo()
+    function foo() public pure override(C, B) returns (string memory) {
+        return super.foo();
+    }
+}
+
+// Inheritance must be ordered from "most base-like" to "most derived".
+// Swapping the order of A and B will throw a compilation error.
+
+contract F is A, B {
+    function foo() public pure override (A, B) returns (string memory) {
+        return super.foo();
+    }
+}

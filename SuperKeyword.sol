ea8a97bb3f4b117719eee6ab2ba5fa99617f9187
common1 <my.common.developer@gmail.com>
2025-01-21 07:37:42 +0100

    38 Solidity Super Keyword | How To Create & Use Super Keyword In Solidity Contract | Solidity Course - Part 2

--- SuperKeyword.sol
+++ SuperKeyword.sol
@@ -17,10 +17,51 @@ contract A {
     event Log(string message);
 
     function foo() public virtual {
-        emit Log("A foo called");
+        emit Log("A.foo called");
     }
 
     function bar() public virtual {
         emit Log("A bar called");
+    }
+}
+
+contract B is A {
+    function foo() public virtual override {
+        emit Log("B.foo called");
+        A.foo();
+    }
+
+    function bar() public virtual override {
+        emit Log("B.bar called");
+        super.bar();
+    }
+}
+
+contract C is A {
+    function foo() public virtual override {
+        emit Log("C.foo called");
+        A.foo();
+    }
+
+    function bar() public virtual override {
+        emit Log("C.bar called");
+        super.bar();
+    }
+}
+
+contract D is B, C {
+    // Try:
+    // - Call D.foo and check the transaction logs.
+    //   although D inherits A, B and C, it only called C and then A.
+    // - Call D.bar and check the transaction logs.
+    //   D called C, then B, and finally A.
+    //   Although super was called twice (by B and C) it only called A once.
+
+    function foo() public override(B, C) {
+        super.foo();
+    }
+
+    function bar() public override(B, C) {
+        super.bar();
     }
 }
